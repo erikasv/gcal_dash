5bf2edc72ed0a02dbd1dc51e1b7c1499fe9d9068
Erika Suárez Valencia <marvinesv@gmail.com>
2020-01-06 01:39:24 -0500

    First very dumb version just to show info

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Prototipo</h1>
+    <p>
+      Listar eventos por investigador que hagan parte de alguno de estos "proyectos":<br />
+      <select v-model="topic">
+        <option value='ALL'>TODOS</option>
+        <option value='ANH'>ANH</option>
+        <option value='ANLA'>ANLA</option>
+      </select>
+    </p>
+    <ul>
+      <li v-for="cal in this.$parent.calendars" v-bind:key="cal.id">
+        {{cal.summary}} <button v-on:click="listEvents(cal.id); selected = cal.id">Load</button>
+      </li>
+    </ul>
+    <div v-if="selected">
+      <h3>Eventos de {{selected}}</h3>
+      <ul>
+        <li v-for="event in events" v-bind:key="event.id">
+          <p>
+            <b>Titulo:</b> {{event.summary}}<br/>
+            <b>Inicio:</b> {{new Date(event.start).toLocaleString('es-CO')}}<br />
+            <b>Finalización:</b> {{new Date(event.end).toLocaleString('es-CO')}}
+          </p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    selected: null,
+    events: [],
+    topic: 'ALL',
+  }),
+  methods: {
+    listEvents (calId) {
+      this.$gapi.getGapiClient()
+        .then(gapi => {
+          const today = new Date()
+          const aMonthAgo = new Date(today.setMonth(today.getMonth() - 1));
+          const aMonthLater = new Date(today.setMonth(today.getMonth() + 2));
+          return gapi.client.calendar.events.list({
+            calendarId: calId,
+            timeMin: aMonthAgo.toISOString(),
+            timeMax: aMonthLater.toISOString(),
+            orderBy: 'startTime',
+            singleEvents: true,
+            maxResults: 25
+          })
+        })
+        .then(info => {
+          if (this.topic !== 'ALL') {
+            console.log(info)
+            return info.result.items.filter(e => e.summary.includes(this.topic))
+          }
+          return info.result.items;
+        })
+        .then(results => {
+          this.events = results.map(e => ({
+            id: e.id,
+            summary: e.summary,
+            start: e.start.dateTime || e.start.date,
+            end: e.end.dateTime || e.end.date,
+          }))
+        })
+        .catch(e => {
+          console.log(e)
+        });
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGAPI from "vue-gapi";
 
+const gApiOpts = {
+  clientId: process.env.VUE_APP_GAPI_CLIENTID,
+  apiKey: process.env.VUE_APP_GAPI_APIKEY,
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  scope: 'https://www.googleapis.com/auth/calendar.readonly',
+};
+
+Vue.use(VueGAPI, gApiOpts);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data: { calendars: [] },
+  mounted () {
+    this.$login()
+      .then(() => {
+        this.$gapi.getGapiClient()
+          .then(gapi => gapi.client.calendar.calendarList.list())
+          .then(cals => {
+            this.calendars = cals.result.items.filter(c => c.id.includes(process.env.VUE_APP_DOMAIN))
+              .map(c => ({ id: c.id, summary: c.summary}));
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      });
+  },
 }).$mount('#app')
